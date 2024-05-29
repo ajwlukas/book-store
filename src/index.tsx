c059ee6f00cbad8a1c0b9ae4da11d81038ac77b5
ajwlukas <ajwlukas@gmail.com>
2024-05-29 15:23:47 +0900

    delete unneeded code

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeContext.Provider value={state}>
     <App />
-    </ThemeContext.Provider>
   </React.StrictMode>
 );

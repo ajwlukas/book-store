990d5127187bfca5c97d4bdbe4b02f8a97dd3cc6
ajwlukas <ajwlukas@gmail.com>
2024-05-30 13:17:17 +0900

    MoveFile

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/Sidebar"
+import Sidebar from "../components/common/Sidebar"
 import Footer from "../components/common/Footer"
 import Header from "../components/common/Header"
 

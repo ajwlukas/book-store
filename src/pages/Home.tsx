b7e83be52be51a1d5ec8b37ccb98d9ce3fad9e78
ajwlukas <ajwlukas@gmail.com>
2024-05-30 13:17:58 +0900

    Created Title and Add Test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
+import Title from "../components/common/Title";
 
 function Home() {
     return (
         <>
+        <Title size="large" color="background">
+            제목 테스트
+        </Title>
         <div>home body</div>
         </>
     )

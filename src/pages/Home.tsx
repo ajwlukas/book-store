9e9ae34a3e9933d711ceda8527d1019b518dd817
ajwlukas <ajwlukas@gmail.com>
2024-06-03 14:14:35 +0900

    added Button component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/common/Button";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 import Title from "../components/common/Title";
@@ -8,6 +9,7 @@ function Home() {
         <Title size="large" color="background">
             제목 테스트
         </Title>
+        <Button size="large" scheme="primary"> 버튼 테스트 </Button>
         <div>home body</div>
         </>
     )
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,13 +1,31 @@
 export type ThemeName = "light" | "dark";
 export type ColorKey = 'primary' | 'background' | 'secondary' | 'third';
-export type HeadingSize = "large" | "medium" | "small" ;
+export type HeadingSize = "large" | "medium" | "small";
+export type ButtonSize = "large" | "medium" | "small";
+export type ButtonScheme = "primary" | "normal";
+
 interface Theme {
     name: string;
     color: Record<ColorKey, string>;
-    heading : {
-        [key in HeadingSize] : {
+    heading: {
+        [key in HeadingSize]: {
             fontSize: string;
         };
+    };
+    button:{
+        [key in ButtonSize]: {
+            fontSize: string;
+            padding: string;
+        };
+    };
+    buttonScheme:{
+        [key in ButtonScheme]: {
+            color: string;
+            backgroundColor: string;
+        };
+    };
+    borderRadius:{
+        default : string;
     }
 }
 
@@ -19,16 +37,44 @@ export const light: Theme = {
         secondary: "blue",
         third: "green",
     },
-    heading : {
-        large :{
+    heading: {
+        large: {
             fontSize: "2rem",
         },
-        medium :{
+        medium: {
             fontSize: "1.5rem",
         },
-        small :{
+        small: {
             fontSize: "1rem",
         },
+    },
+    button:{
+        large: {
+            fontSize: "2rem",
+            padding: "1rem 2rem"
+        },
+        medium: {
+            fontSize: "1.5rem",
+            padding: "0.5rem 1rem"
+        },
+        small: {
+            fontSize: "1rem",
+            padding: "0.25rem 0.5rem"
+        },
+
+    },
+    buttonScheme:{
+        primary: {
+            color: "white",
+            backgroundColor: "midnightblue"
+        },
+        normal: {
+            color: "black",
+            backgroundColor: "lightgray"
+        }
+    },
+    borderRadius:{
+        default : "4px",
     }
 };
 

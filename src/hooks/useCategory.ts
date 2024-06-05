bf76b1916912edff9bb19e855ab83cfa69eb4308
ajwlukas <ajwlukas@gmail.com>
2024-06-05 23:13:12 +0900

    created book filter

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -1,24 +1,55 @@
 import { useEffect, useState } from "react";
 import { Category } from "../models/category.model";
 import { fetchCategory } from "../api/category.api";
+import { useLocation } from "react-router-dom";
 
-export const useCategory = () =>{
+export const useCategory = () => {
+    const location = useLocation();//쿼리를 따주는 함수 location.search
     const [categories, setCategories] = useState<Category[]>([]);
+
+    const setActive = () => {
+        const params = new URLSearchParams(location.search);
+        if (params.get('category_id')) {
+            setCategories((prev) => {
+                return prev.map((category) => {
+                    return category.id === Number(params.get('category_id'))
+                        ? { ...category, isActive: true }
+                        : { ...category, isActive: false }
+                })
+            })
+        }
+        else
+        {
+            setCategories((prev) => {
+                return prev.map((category) => {
+                    return category.id === null
+                        ? { ...category, isActive: true }
+                        : { ...category, isActive: false }
+                })
+            })
+        }
+    }
 
     useEffect(() => {
         fetchCategory().then((categories) => {
 
             const categoryWithAll = [
                 {
-                    id : null,
-                    name : "전체",
+                    id: null,
+                    name: "전체",
+                    isActive: false,
                 },
                 ...categories,
             ];
 
             setCategories(categoryWithAll);
+            setActive();
         })
     }, []);
 
-    return {categories};
+    useEffect(()=>{
+        setActive();
+    },[location.search]);
+
+    return { categories };
 }
